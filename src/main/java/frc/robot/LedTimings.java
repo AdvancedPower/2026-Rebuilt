3c73c3e11cc7a5f0869801a61edf671d3f5ebec5
Brian Wirsing <beamer159@gmail.com>
2026-01-27 19:43:47 -0500

    Added end game warning

--- src/main/java/frc/robot/LedTimings.java
+++ src/main/java/frc/robot/LedTimings.java
@@ -31,23 +31,18 @@ public class LedTimings {
         final double phase2Start = phase3Start + phaseLength;
         final double phase1Start = phase2Start + phaseLength;
         final double transitionStart = phase1Start + 10;
-
         final double inactiveWarningTime = 3;
         final double activeWarningTime = 5;
-
+        final double scrollSpeed = 50;
+        final double blinkSpeed = 0.4;
         final Color activeColor = Color.kGreen;
         final Color inactiveColor = Color.kRed;
         final Color inactiveWarningColor = Color.kOrange;
         final Color activeWarningColor = Color.kBlue;
-
         final PhaseData activeData = new PhaseData(activeColor, activeWarningColor, activeWarningTime);
         final PhaseData inactiveData = new PhaseData(inactiveColor, inactiveWarningColor, inactiveWarningTime);
 
-        new Trigger(DriverStation::isAutonomousEnabled).onTrue(leds.runPattern(Leds.scroll(activeColor, 50)));
-
-        new Trigger(() -> DriverStation.isTeleopEnabled() && DriverStation.getMatchTime() <= transitionStart)
-            .onTrue(leds.runPattern(Leds.scroll(activeColor, 50)));
-
+        // Determine Phase 1/3 and 2/4 colors and timings
         new Trigger(() -> DriverStation.getGameSpecificMessage().isEmpty()).onFalse(Commands.runOnce(() -> {
             var inactiveFirstChar = DriverStation.getGameSpecificMessage(); // 'R' (red) or 'B' (blue)
             var myAlliance = DriverStation.getAlliance();
@@ -56,31 +51,51 @@ public class LedTimings {
             phase2And4Data = inactiveFirst ? activeData : inactiveData;
         }));
 
-        new Trigger(() -> phase1And3Data != null && DriverStation.getMatchTime() < phase1Start + phase1And3Data.warningTime && phase1And3Data.phaseColor == inactiveColor)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase1And3Data.warningColor, 0.4))));
+        // Auto - always active
+        new Trigger(DriverStation::isAutonomousEnabled).onTrue(leds.runPattern(Leds.scroll(activeColor, scrollSpeed)));
 
+        // Transition - always active
+        new Trigger(() -> DriverStation.isTeleopEnabled() && DriverStation.getMatchTime() <= transitionStart)
+            .onTrue(leds.runPattern(Leds.scroll(activeColor, scrollSpeed)));
+
+        // Phase 1 warning - Only used if we are inactive during phase 1
+        new Trigger(() -> phase1And3Data != null && DriverStation.getMatchTime() < phase1Start + inactiveWarningTime && phase1And3Data.phaseColor == inactiveColor)
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(inactiveWarningColor, blinkSpeed))));
+
+        // Phase 1
         new Trigger(() -> phase1And3Data != null && DriverStation.getMatchTime() < phase1Start)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase1And3Data.phaseColor, 50))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase1And3Data.phaseColor, scrollSpeed))));
 
+        // Phase 2 warning
         new Trigger(() -> phase2And4Data != null && DriverStation.getMatchTime() < phase2Start + phase2And4Data.warningTime)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase2And4Data.warningColor, 0.4))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase2And4Data.warningColor, blinkSpeed))));
 
+        // Phase 2
         new Trigger(() -> phase2And4Data != null && DriverStation.getMatchTime() < phase2Start)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase2And4Data.phaseColor, 50))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase2And4Data.phaseColor, scrollSpeed))));
 
+        // Phase 3 warning
         new Trigger(() -> phase1And3Data != null && DriverStation.getMatchTime() < phase3Start + phase1And3Data.warningTime)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase1And3Data.warningColor, 0.4))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase1And3Data.warningColor, blinkSpeed))));
 
+        // Phase 3
         new Trigger(() -> phase1And3Data != null && DriverStation.getMatchTime() < phase3Start)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase1And3Data.phaseColor, 50))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase1And3Data.phaseColor, scrollSpeed))));
 
+        // Phase 4 warning
         new Trigger(() -> phase2And4Data != null && DriverStation.getMatchTime() < phase4Start + phase2And4Data.warningTime)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase2And4Data.warningColor, 0.4))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(phase2And4Data.warningColor, blinkSpeed))));
 
+        // Phase 4
         new Trigger(() -> phase2And4Data != null && DriverStation.getMatchTime() < phase4Start)
-            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase2And4Data.phaseColor, 50))));
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.scroll(phase2And4Data.phaseColor, scrollSpeed))));
 
+        // End game warning - Only used if we are inactive during phase 4
+        new Trigger(() -> phase2And4Data != null && DriverStation.getMatchTime() < endGameStart + activeWarningTime && phase2And4Data.phaseColor == inactiveColor)
+            .onTrue(leds.defer(() -> leds.runPattern(Leds.blink(activeWarningColor, blinkSpeed))));
+
+        // End game
         new Trigger(() -> DriverStation.isTeleopEnabled() && DriverStation.getMatchTime() < endGameStart)
-            .onTrue(leds.runPattern(Leds.scroll(activeColor, 50)));
+            .onTrue(leds.runPattern(Leds.scroll(activeColor, scrollSpeed)));
     }
 }
